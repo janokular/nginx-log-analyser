9b9bd5c115e29a7bb42b7d590c31cd15932120ea
janekokularczyk@gmail.com <janekokularczyk@gmail.com>
2025-03-09 18:30:31 +0100

    staged changes

--- analyser.sh
+++ analyser.sh
@@ -16,20 +16,20 @@ fi
 
 # Top 5 IP addresses with the most requests
 echo "Top 5 IP addresses with the most requests:"
-cut -d " " -f 1 "${LOG_FILE}" | sort | uniq -c | sort -rn | head -5 | awk '{ print $2, $1 }' | column -t
+cut -d " " -f 1 "${LOG_FILE}" | sort | uniq -c | sort -rn | head -5 | awk '{ print $2, "-", $1, "requests" }'
 echo
 
 # Top 5 most requested paths
 echo "Top 5 most requested paths:"
-cut -d " " -f 7 "${LOG_FILE}" | sort | uniq -c | sort -rn | head -5 | awk '{ print $2, $1 }' | column -t
+cut -d " " -f 7 "${LOG_FILE}" | sort | uniq -c | sort -rn | head -5 | awk '{ print $2, "-", $1, "requests" }'
 echo
 
 # Top 5 response status codes
 echo "Top 5 response status codes:"
-cut -d '"' -f 3 "${LOG_FILE}" | cut -d " " -f 2 | sort | uniq -c | sort -rn | head -5 | awk '{ print $2, $1 }' | column -t
+cut -d '"' -f 3 "${LOG_FILE}" | cut -d " " -f 2 | sort | uniq -c | sort -rn | head -5 | awk '{ print $2, "-", $1, "requests" }'
 echo
 
 # Top 5 user agents
 echo "Top 5 user agents:"
-cut -d '"' -f 6 "${LOG_FILE}" | sort | uniq -c | sort -rn | head -5
+cut -d '"' -f 6 "${LOG_FILE}" | sort | uniq -c | sort -rn | head -5 | awk '{ first = $1; $1=""; print $0, "-", first, "requests" }' | cut -c 2-
 echo
